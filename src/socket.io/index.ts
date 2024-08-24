65d9d41b6278157a39161be5c5cf5f593f47079c
zinmgmg <zinmgmg.edka@gmail.com>
2024-08-24 18:27:30 +0630

    fixed cross origin to *

--- src/socket.io/index.ts
+++ src/socket.io/index.ts
@@ -4,7 +4,7 @@ import { Server as HTTPServer } from "http";
 export default function setupSocketIO(server: HTTPServer): SocketIOServer {
     const io = new SocketIOServer(server, {
         cors: {
-            origin: `http://localhost:3000`,
+            origin: `*`,
             methods: ["GET", "POST"],
         },
     });
@@ -12,7 +12,7 @@ export default function setupSocketIO(server: HTTPServer): SocketIOServer {
     io.on("connection", (socket) => {
         console.log(`New client connected: ${socket.id}`);
 
-        socket.on("emp-current-location", (locationData:any) => {
+        socket.on("driver-current-location", (locationData:any) => {
             console.log("Received client location:", locationData);
             // You can process or save the location data here
         });
